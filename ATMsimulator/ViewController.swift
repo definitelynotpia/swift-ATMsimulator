77fde093901769c64f985311aa7579d2c56c24f2
Frances Pia Alexy Rey <frances.rey@ciit.edu.ph>
2024-05-03 15:14:43 +0800

    Initial Commit

---
+++ ATMsimulator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATMsimulator
+//
+//  Created by Guest User on 5/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
